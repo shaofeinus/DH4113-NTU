605a5d797df67870d28bea7a56461836ab83e392
unknown <CX>
2015-09-18 14:55:30 +0800

    Initial commit of point to point Navigation

---
+++ Pi/map/testJsonParsing.py
@@ -0,0 +1,14 @@
+from jsonParsing import mapParser
+
+com1L2 = mapParser("com1L2")
+com2L2 = mapParser("com2L2")
+com2L3 = mapParser("com2L3")
+
+for i in range(com1L2.numElements):
+    print com1L2.getLocationName(i)
+
+for i in range(com2L2.numElements):
+    print com2L2.getLocationName(i)
+
+for i in range(com2L3.numElements):
+    print com2L3.getLocationName(i)
