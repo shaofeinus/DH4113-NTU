c33bdeddcf0b22c31f2817873ee19c7960028077
unknown <CX>
2015-10-10 14:45:11 +0800

    Modularized Search

---
+++ Pi/UI/testSearch.py
@@ -0,0 +1,13 @@
+import search
+
+startPoint = search.locationSetting()
+
+
+##print "Start building is: " + str(startPoint.getBuildingName())
+##print "Start level number is: " + str(startPoint.getLevelNumber())
+##print "Start point is: " + str(startPoint.getLocationPointIndex())
+
+endPoint = search.locationSetting()
+##print "End building is: " + str(endPoint.getBuildingName())
+##print "End level number is: " + str(endPoint.getLevelNumber())
+##print "End point is: " + str(endPoint.getLocationPointIndex())
