1597c4bc3486204c33a843f98e6d24c7abedfd71
unknown <CX>
2015-11-12 13:45:44 +0800

    update locationTracker north difference

--- Pi/mainThread.py
+++ Pi/mainThread.py
@@ -559,8 +559,14 @@ class NavigationThread(threading.Thread):
                 if navi.hasNextPath() is True :
                     isNextPathNeeded = True
                     navi.switchToNextPathList()
+                    # update location tracker initial info
+
+                    locationTrackerLock.acquire()
+                    locationTracker.updateMapNorth(navi.getNorthDifference())
                     # navi.getNorthDifference()
                     # navi.getFirstCoordinates()
+                    locationTrackerLock.release()
+
                     print "press start to continue"
                     UISpeaker.speak("Now entering new map. Press start to continue.")
                     while keypad.get_binary_response():
